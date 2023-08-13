f92e163b2aa5b55e6f379e60a938b49f4ee166cf
Chris Mbah <mbahchris46@gmail.com>
2023-08-13 18:13:11 +0100

    Implemented Quiz UI

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { QuizApp } from "../../App";
+import { useContext } from "react";
+import ComicsQuiz from "../QuizCategories/ComicsQuiz";
+import HistoryQuiz from "../QuizCategories/HistoryQuiz";
+import MathQuiz from "../QuizCategories/MathQuiz";
+import ScienceQuiz from "../QuizCategories/ScienceQuiz";
 
 export default function Quiz() {
+  const { quizCategory } = useContext(QuizApp);
   return (
-    <div>Quiz</div>
-  )
+    <div className="quiz">
+      {quizCategory === "comics" && <ComicsQuiz />}
+      {quizCategory === "science" && <ScienceQuiz />}
+      {quizCategory === "history" && <HistoryQuiz />}
+      {quizCategory === "maths" && <MathQuiz />}
+    </div>
+  );
 }
