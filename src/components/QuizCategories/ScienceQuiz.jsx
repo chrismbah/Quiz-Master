a74db0e0099bcac36d1b66acfd3df1993071e87a
Chris Mbah <mbahchris46@gmail.com>
2023-08-13 19:39:54 +0100

    Science category implementation

--- src/components/QuizCategories/ScienceQuiz.jsx
+++ src/components/QuizCategories/ScienceQuiz.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Science } from "../../helpers/Science";
 
 export default function ScienceQuiz() {
+  const [curr, setCurr] = useState(1);
   return (
-    <div>ScienceQuiz</div>
-  )
+    <div className="science">
+      <div className="box">
+        <div className="question">
+          <h3>{Science[curr].question}</h3>
+        </div>
+        <div className="options">
+          <div className="option">
+            <h3>A.</h3>
+            <button>{Science[curr].A}</button>
+          </div>
+          <div className="option">
+            <h3>B.</h3>
+            <button>{Science[curr].B}</button>
+          </div>{" "}
+          <div className="option">
+            <h3>C.</h3>
+
+            <button>{Science[curr].C}</button>
+          </div>{" "}
+          <div className="option">
+            <h3>D.</h3>
+
+            <button>{Science[curr].D}</button>
+          </div>{" "}
+        </div>
+        <div className="action-button">
+        <button >
+          Continue <i class="fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+      </div>
+    </div>
+  );
 }
