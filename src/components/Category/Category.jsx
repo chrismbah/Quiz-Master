c5fa0bfdf87b311d789b286cb5230a4b458231cd
Chris Mbah <mbahchris46@gmail.com>
2023-08-13 18:12:27 +0100

    Category change functionality

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Category.css";
+import { QuizApp } from "../../App";
+import { useContext } from "react";
 
 export default function Category() {
+  const {quizState,setQuizState,quizCategory,setQuizCategory}=useContext(QuizApp)
+  function handleClick(){
+    if(quizCategory===""){
+      alert("Please pick a category")
+    }
+    else{
+      setQuizState("quiz")
+    }
+  }
   return (
     <div className="category">
       <div className="box">
@@ -9,22 +20,22 @@ export default function Category() {
           <h2>Select A Category of Your Choice </h2>
         </div>
         <div className="select-options">
-          <button>
+          <button onClick={()=>setQuizCategory("science")}>
            <span>Science </span> <i class="fa-solid fa-flask fa-bounce"></i>{" "}
           </button>
-          <button>
+          <button onClick={()=>setQuizCategory("maths")}>
            <span> Mathematics</span><i class="fa-solid fa-calculator fa-bounce"></i>{" "}
           </button>
-          <button>
+          <button onClick={()=>setQuizCategory("history")}>
           <span> History</span>{" "}
             <i class="fa-solid fa-clock-rotate-left fa-spin fa-spin-reverse"></i>{" "}
           </button>
-          <button>
+          <button onClick={()=>setQuizCategory("comics")}>
           <span> Comics</span><i class="fa-solid fa-mask fa-bounce"></i>
           </button>
         </div>
         <div className="action-button">
-          <button>
+          <button onClick={handleClick}>
             <span>Next</span> <i class="fa-solid fa-arrow-right"></i>
           </button>
         </div>
