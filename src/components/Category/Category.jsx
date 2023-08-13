c1a00fc1903ac1d72b001cc63955c03386b816c7
Chris Mbah <mbahchris46@gmail.com>
2023-08-13 14:20:40 +0100

    Category component for options

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,25 +1,33 @@
 import React from "react";
+import "./Category.css";
 
 export default function Category() {
   return (
     <div className="category">
-      <div className="select-text">
-        <h2>Select A Category </h2>
-      </div>
-      <div className="select-options">
-        <button>
-          Science <i class="fa-solid fa-flask fa-bounce"></i>{" "}
-        </button>
-        <button>
-          Mathematics<i class="fa-solid fa-calculator fa-bounce"></i>{" "}
-        </button>
-        <button>
-          History{" "}
-          <i class="fa-solid fa-clock-rotate-left fa-spin fa-spin-reverse"></i>{" "}
-        </button>
-        <button>
-          Comics<i class="fa-solid fa-mask fa-bounce"></i>
-        </button>
+      <div className="box">
+        <div className="select-text">
+          <h2>Select A Category of Your Choice </h2>
+        </div>
+        <div className="select-options">
+          <button>
+           <span>Science </span> <i class="fa-solid fa-flask fa-bounce"></i>{" "}
+          </button>
+          <button>
+           <span> Mathematics</span><i class="fa-solid fa-calculator fa-bounce"></i>{" "}
+          </button>
+          <button>
+          <span> History</span>{" "}
+            <i class="fa-solid fa-clock-rotate-left fa-spin fa-spin-reverse"></i>{" "}
+          </button>
+          <button>
+          <span> Comics</span><i class="fa-solid fa-mask fa-bounce"></i>
+          </button>
+        </div>
+        <div className="action-button">
+          <button>
+            <span>Next</span> <i class="fa-solid fa-arrow-right"></i>
+          </button>
+        </div>
       </div>
     </div>
   );
