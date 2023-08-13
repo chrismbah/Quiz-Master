70571bf31eca47befa19ce39780ce3db151ebfad
Chris Mbah <mbahchris46@gmail.com>
2023-08-13 19:30:33 +0100

    Gave modules unique numbers for identification of number of question

---
+++ src/helpers/Comics.jsx
@@ -0,0 +1,203 @@
+export const Comics = [
+    {
+      no: 1,
+      question:
+        "What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 2,
+      question:
+        " Which of the following colors of light has the shortest wavelength?",
+      A: " Red",
+      B: "Blue",
+      C: "Yellow",
+      D: "Cyan",
+      answer: "B",
+    },
+    {
+      no: 3,
+      question:
+        " Which law states that for every action, there is an equal and opposite reaction?",
+      A: "Newton's First Law",
+      B: "Newton's Second Law",
+      C: "Newton's Third Law",
+      D: "Law Of Universal Gravitation",
+      answer: "C",
+    },
+    {
+      no: 4,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 5,
+  
+      question: "Which fundamental particle has a positive charge?",
+      A: "Proton",
+      B: "Neutron",
+      C: "Electron",
+      D: "Quark",
+      answer: "A",
+    },
+    {
+      no: 6,
+      question:
+        "Which phenomenon causes a sound to become more faint as it moves away from its source?",
+      A: "Diffreaction",
+      B: "Refraction",
+      C: "Reflection",
+      D: "Attenuation",
+      answer: "D",
+    },
+    {
+      no: 7,
+      question:
+        "Question: What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 8,
+      question:
+        "Question: What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 9,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 10,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 11,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 12,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 13,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 14,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 15,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 16,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 17,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 18,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 19,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+    {
+      no: 20,
+      question:
+        " What is the unit of measurement for electric potential difference?",
+      A: "Watts",
+      B: "Joules",
+      C: "Volts",
+      D: "Coulombs",
+      answer: "C",
+    },
+  ];
+  
