8845aa5f1df88ac671838ec33ebd90c7522ab713
Chris Mbah <mbahchris46@gmail.com>
2023-08-14 22:22:15 +0100

    Minor changes

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -4,13 +4,13 @@ import { QuizApp } from "../../App";
 import { useContext } from "react";
 
 export default function Category() {
-  const {setQuizState,quizCategory,setQuizCategory}=useContext(QuizApp)
-  function handleClick(){
-    if(quizCategory===""){
-      alert("Please pick a category")
-    }
-    else{
-      setQuizState("quiz")
+  const { setQuizState, quizCategory, setQuizCategory, setScore } =
+    useContext(QuizApp);
+  function handleClick() {
+    if (quizCategory === "") {
+      alert("Please pick a category");
+    } else {
+      setQuizState("quiz");
     }
   }
   return (
@@ -20,18 +20,20 @@ export default function Category() {
           <h2>Select A Category of Your Choice </h2>
         </div>
         <div className="select-options">
-          <button onClick={()=>setQuizCategory("science")}>
-           <span>Science </span> <i class="fa-solid fa-flask fa-bounce"></i>{" "}
+          <button onClick={() => setQuizCategory("science")}>
+            <span>Science </span> <i class="fa-solid fa-flask fa-bounce"></i>{" "}
           </button>
-          <button onClick={()=>setQuizCategory("maths")}>
-           <span> Mathematics</span><i class="fa-solid fa-calculator fa-bounce"></i>{" "}
+          <button onClick={() => setQuizCategory("maths")}>
+            <span> Mathematics</span>
+            <i class="fa-solid fa-calculator fa-bounce"></i>{" "}
           </button>
-          <button onClick={()=>setQuizCategory("history")}>
-          <span> History</span>{" "}
+          <button onClick={() => setQuizCategory("history")}>
+            <span> History</span>{" "}
             <i class="fa-solid fa-clock-rotate-left fa-spin fa-spin-reverse"></i>{" "}
           </button>
-          <button onClick={()=>setQuizCategory("comics")}>
-          <span> Comics</span><i class="fa-solid fa-mask fa-bounce"></i>
+          <button onClick={() => setQuizCategory("comics")}>
+            <span> Comics</span>
+            <i class="fa-solid fa-mask fa-bounce"></i>
           </button>
         </div>
         <div className="action-button">
