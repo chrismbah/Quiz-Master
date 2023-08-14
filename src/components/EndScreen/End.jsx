a613885821ff7c374e29bda0c59ede89fabb2193
Chris Mbah <mbahchris46@gmail.com>
2023-08-14 12:54:33 +0100

    End screen UI for ending or restarting quiz

--- src/components/EndScreen/End.jsx
+++ src/components/EndScreen/End.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { QuizApp } from "../../App";
 
 export default function End() {
+  const { score,setQuizState } = useContext(QuizApp);
   return (
-    <div>End</div>
-  )
+    <div className="end">
+      <div className="box">
+        <div className="end-text">
+          <h2>Quiz Finished!</h2>
+        </div>
+        <div className="score">
+          <h3>
+            Score: {score}/{20}
+          </h3>
+        </div>
+        <div className="restart">
+          <button onClick={()=>setQuizState("category")}>Restart Quiz</button>
+          <button onClick={()=>setQuizState("menu")}>Exit</button>
+        </div>
+      </div>
+    </div>
+  );
 }
