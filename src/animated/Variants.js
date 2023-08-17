f8cd1ed5de8ae34e3ce059b55dd88551d016fbc7
Chris Mbah <mbahchris46@gmail.com>
2023-08-18 00:40:51 +0100

    Variants module for framer motion animation

---
+++ src/animated/Variants.js
@@ -0,0 +1,21 @@
+export const Variants = {
+    hidden: { opacity: 0, y: 70 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 200,
+        damping: 15,
+      },
+    },
+    hover: {
+      scale: 1.1,
+      transition: {
+        type: "spring",
+        duration: 0.3,
+        stiffness: 200,
+        damping: 15,
+      },
+    },
+  };
